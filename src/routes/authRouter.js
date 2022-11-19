10f7922d0da86d2d7919389d94713fdb4adc90e1
Saulo <22698083+saulodigital@users.noreply.github.com>
2022-11-19 21:00:49 +0000

    v2

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -5,18 +5,18 @@ const router = Router()
 
 router.get('/discord', passport.authenticate('discord'), (req, res) =>
   res.sendStatus(200)
-);
+)
 
 router.get('/discord/redirect', passport.authenticate('discord'), (req, res) =>
   // to update when frontend is done
-  // res.send({ msg: 'Success' })
-  res.redirect(`${ATLAS_URL}/dashboard`)
-);
+  // res.redirect(`${ATLAS_URL}/dashboard`)
+  res.send({ msg: 'Success' })
+)
 
 router.get('/status', (req, res) => {
   return req.user
     ? res.send(req.user)
     : res.status(401).send({ msg: 'Unauthorized' })
-});
+})
 
 export default router
